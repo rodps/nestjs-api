c39a68b26d3811eb2fcc195df8c480dd183b5de3
Rodrigo da Silva <rodrigopds73@gmail.com>
2025-02-21 20:14:22 -0300

    test: add create tests for admin controller

--- src/admin/admin.controller.spec.ts
+++ src/admin/admin.controller.spec.ts
@@ -3,6 +3,8 @@ import { Test } from '@nestjs/testing';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { Admin } from './admin.entity';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import { RequestMethod } from '@nestjs/common';
 
 describe('Admin Controller', () => {
   let adminController: AdminController;
@@ -52,6 +54,53 @@ describe('Admin Controller', () => {
       jest.spyOn(adminService, 'getDetailsById').mockRejectedValue(new Error());
 
       await expect(adminController.getDetails(1)).rejects.toThrow();
+    });
+  });
+
+  describe('create', () => {
+    it('should call save from AdminService', async () => {
+      const adminDto = new CreateAdminDto();
+      jest.spyOn(adminService, 'save').mockResolvedValue();
+
+      await adminController.create(adminDto);
+
+      expect(adminService.save).toHaveBeenCalledWith(adminDto.toEntity());
+    });
+
+    it('should return the created admin', async () => {
+      const adminDto = new CreateAdminDto();
+      adminDto.email = 'test@email.com';
+      const admin = adminDto.toEntity();
+      jest.spyOn(adminService, 'save').mockResolvedValue();
+
+      const result = await adminController.create(adminDto);
+
+      expect(result).toEqual(admin);
+    });
+
+    it('should throw an error if admin is not created', async () => {
+      const adminDto = new CreateAdminDto();
+      jest.spyOn(adminService, 'save').mockRejectedValue(new Error());
+
+      await expect(adminController.create(adminDto)).rejects.toThrow();
+    });
+
+    it('should be public', () => {
+      expect(
+        Reflect.getMetadata('isPublic', AdminController.prototype.create),
+      ).toBe(true);
+    });
+
+    it('should be a post method', () => {
+      expect(
+        Reflect.getMetadata('method', AdminController.prototype.create),
+      ).toBe(RequestMethod.POST);
+    });
+
+    it('should have the correct path', () => {
+      expect(
+        Reflect.getMetadata('path', AdminController.prototype.create),
+      ).toBe('/');
     });
   });
 });
