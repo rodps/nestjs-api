e896bf8739e6d6164e0cef0e2a81b192a9a0ae02
Rodrigo da Silva <rodrigopds73@gmail.com>
2025-02-21 20:47:07 -0300

    refactor: remove app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,9 +7,8 @@ import { AuthGuard } from './auth/auth.guard';
 
 @Module({
   imports: [MikroOrmModule.forRoot(config), AdminModule, AuthModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     {
       provide: 'APP_GUARD',
       useClass: AuthGuard,
