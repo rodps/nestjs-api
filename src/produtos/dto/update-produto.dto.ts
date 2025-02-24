f1387064fe44ff32421a695a700447718129d09f
Rodrigo da Silva <rodrigopds73@gmail.com>
2025-02-23 21:09:55 -0300

    refactor(produtos): remove method toEntity from dto

--- src/produtos/dto/update-produto.dto.ts
+++ src/produtos/dto/update-produto.dto.ts
@@ -1,5 +1,4 @@
 import { IsNotEmpty } from 'class-validator';
-import { Produto } from '../produto.entity';
 
 export class UpdateProdutoDto {
   @IsNotEmpty({ message: 'O campo nome é obrigatório' })
@@ -10,12 +9,4 @@ export class UpdateProdutoDto {
 
   @IsNotEmpty({ message: 'O campo estoque é obrigatório' })
   estoque: number;
-
-  toEntity(): Produto {
-    const produto = new Produto();
-    produto.nome = this.nome;
-    produto.preco = this.preco;
-    produto.estoque = this.estoque;
-    return produto;
-  }
 }
